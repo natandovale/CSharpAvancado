cbc9fe72760d1f28307265029892fdf513126ba1
natanaeloliveira <natanaeloliveira@mobills.com.br>
2022-04-22 17:13:12 -0300

    Aulas 1 e 2

---
+++ Aula1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Escrevendo Código em Execução
+using System.Reflection.Emit;
+
+var intType = typeof(int);
+var assinatura = typeof(Func<int, int, int>);
+
+Func<int, int, int> soma = Soma;
+
+var dynamicMethod = new DynamicMethod("SomaCriadaEmExecucao", intType, new[] { intType, intType});
+
+var ilGenerator = dynamicMethod.GetILGenerator();
+ilGenerator.Emit(OpCodes.Ldarg_0);
+ilGenerator.Emit(OpCodes.Ldarg_1);
+ilGenerator.Emit(OpCodes.Add);
+ilGenerator.Emit(OpCodes.Ret);
+
+var somaEmExecucao = (Func<int, int, int>)dynamicMethod.CreateDelegate(assinatura);
+
+Console.WriteLine("Soma de 1+2 definido em desenvolvimento é {0}", Soma(1, 2));
+Console.WriteLine("Soma de 1+2 definido em Execução é {0}", somaEmExecucao(1, 2));
+
+static int Soma(int valorA, int valorB)
+{
+    return valorA + valorB;
+}
---
+++ Aula2/Program.cs
@@ -0,0 +1,39 @@
+﻿foreach (var i in EnumerableNumerosDe1a10())
+{
+    Console.WriteLine(i);
+}
+
+var enumeratorNumerosDe1A10 = EnumeratorNumerosDe1a10();
+
+while (enumeratorNumerosDe1A10.MoveNext())
+{
+    Console.WriteLine(enumeratorNumerosDe1A10.Current);
+}
+
+static IEnumerable<int> EnumerableNumerosDe1a10()
+{
+    yield return 1;
+    yield return 2;
+    yield return 3;
+    yield return 4;
+    yield return 5;
+    yield return 6;
+    yield return 7;
+    yield return 8;
+    yield return 9;
+    yield return 10;
+}
+
+static IEnumerator<int> EnumeratorNumerosDe1a10()
+{
+    yield return 1;
+    yield return 2;
+    yield return 3;
+    yield return 4;
+    yield return 5;
+    yield return 6;
+    yield return 7;
+    yield return 8;
+    yield return 9;
+    yield return 10;
+}
